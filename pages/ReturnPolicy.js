5cee68c51d462d1c75a15d2071ba16289ed0bb8a
shanza.amin@tkxel.io <shanza.amin@tkxel.io>
2023-02-02 00:32:20 +0500

    pages in progress

---
+++ pages/ReturnPolicy.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "@/components/ui/Container";
+
+const ReturnPolicy = () => {
+    return(
+        <>
+
+<Container> 
+<div>
+
+MyridePAY is an online platform that connects the customers with their ideal cars available. Once a customer enters a contract with the car rental company at the time of Pickup/Delivery they accept to follow and obey the terms and policies of that specific company including their refund policy. myridePAY holds the responsibility towards the fund of a client until the moment a customer enters an agreement with the car rental company. Once entered myridePAY does not hold liability towards the funds of a customer as myridePAY forwards the funds to the Car rental company.
+</div>
+
+</Container>
+        </>
+    )
+}
+export default ReturnPolicy;
